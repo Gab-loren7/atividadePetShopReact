b9cbb7fd256f7766a3b01dd56a4d58f2fb21b1e8
GABRIEL CARVALHO LORENSSETTI <gabriel_lorenssetti@estudante.sesisenai.org.br>
2025-05-26 11:47:22 -0300

    upload final

---
+++ petShop-main/src/pages/HotelResort.js
@@ -0,0 +1,11 @@
+import "../components/identificadorDaPagina.css"
+
+
+function HotelResort(){
+    return(
+        <h1 className="identificacaoPagina">Hotel Resort</h1>
+
+    )
+}
+
+export default HotelResort
