fba11b38f764f41abe4c1006c4a14df8e3a1e3f8
GABRIEL CARVALHO LORENSSETTI <gabriel_lorenssetti@estudante.sesisenai.org.br>
2025-05-26 09:26:06 -0300

    upload

---
+++ petShop-main/src/pages/Adestramento.js
@@ -0,0 +1,9 @@
+import "../components/identificadorDaPagina.css"
+
+function Adestramento(){
+    return(
+        <h1 className="identificacaoPagina">Adestramento</h1>
+    )
+}
+
+export default Adestramento
