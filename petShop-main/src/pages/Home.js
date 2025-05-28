42e0adbd70c1ceb1a5def675095d3d241e7497a2
Gabriel <gabriel_lorenssetti@estudante.sesisenai.org.br>
2025-05-28 12:23:27 -0300

    upload final

---
+++ petShop-main/src/pages/Home.js
@@ -0,0 +1,13 @@
+import Banner from "../components/Banner"
+import Footer from "../components/footer"
+
+function Home(){
+    return(
+        <div>
+            <Banner />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
